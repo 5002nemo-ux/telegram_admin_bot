3321ff263d03c7af41b40256ecc38c3df64e5e22
5002nemo-ux <5002nemo@gmail.com>
2026-07-14 23:33:03 +0500

    Add config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8621541271:AAF3eGKlL62HCXOXfjRmxVZUT4g_i0iZAzc")
+# Sizning Telegram ID raqamingizni kiritish kerak, xabar tarqatish (broadcast) funksiyasidan foydalanish uchun.
+# Botga shaxsiy xabarda (lichkada) /id deb yozsangiz sizga ID ni beradi. Uni shu yerga yozib qo'yasiz.
+OWNER_ID = 5410966675
